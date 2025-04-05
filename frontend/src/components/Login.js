f3c29fc92bfa79160a44bd35f064e2c113161f53
pradhumnaDsawalkar <pradhumna0007@gmail.com>
2025-04-05 19:31:46 +0530

    Deployment

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(' http://localhost:5000/api/login', {
+      const response = await fetch('https://hospital-management-system-de1m.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -38,7 +38,7 @@ const SignUp = () => {
     e.preventDefault();
     if (validateForm()) {
       try {
-        const response = await fetch(' http://localhost:5000/api/signup', {
+        const response = await fetch('https://hospital-management-system-de1m.onrender.com/api/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
